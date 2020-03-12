45744f712f943a56ea6703a2060bb89ce4e2b260
rubenpazch <rubenpazchuspe@outlook.com>
2020-03-11 23:00:40 -0500

    adding repeating string solution

---
+++ Repeated-String.rb
@@ -0,0 +1,21 @@
+def repeatedString(s, n) #abba, 14
+  len = s.length #4
+  div = n / len #3
+  dif = n - (div * len)
+
+  array = s.chars
+  totalA = array.select { |item| item == "a" }
+
+  if (dif == 0)
+    return totalA.length * div
+  else
+    modString = s[0, dif]
+    puts modString
+    shortArray = modString.chars
+    totalMod = shortArray.select { |item| item == "a" }
+    return totalA.length * div + totalMod.length
+  end
+end
+
+puts repeatedString("abbac", 14)
+#abbac-abbac-abba
