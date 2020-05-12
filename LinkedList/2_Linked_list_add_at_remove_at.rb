3579b79698b2f13e4a1f80202d24132d0e332543
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-05-11 21:47:49 -0500

    change name file

--- LinkedList/2_Linked_list_add_at_remove_at.rb
+++ LinkedList/2_Linked_list_add_at_remove_at.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Start with your code from last challenge.
 class Node
   attr_accessor :value, :next_node
@@ -11,61 +13,83 @@ class LinkedList
   attr_accessor :head, :tail
 
   def add(number)
-    new_node = Node.new()
+    new_node = Node.new
     new_node.value = number
-    new_node.next_node=nil
-    
+    new_node.next_node = nil
+
     if head.nil?
-      @head=new_node
-      @tail=new_node
-    else 
+      @head = new_node
+      @tail = new_node
+    else
       @tail.next_node = new_node
       @tail = new_node
     end
   end
-  
+
   def get(index)
     i = 0
     temp = head
     until temp.nil?
       return temp.value if index == i
+
       temp = temp.next_node
       i += 1
     end
     0
   end
-  
-  def add_at(index,number)
+
+  def remove(index)
+    return if index < 0
+
+    if index.zero?
+      temp = head.next_node
+      @head = temp
+      return
+    end
+
+    if index == 1
+      temp = head.next_node.next_node
+      @head.next_node = temp
+    else
+      i = 0
+      current = head
+      while i < index - 1 && !current.next_node.nil?
+        current = current.next_node
+        i += 1
+      end
+      temp = current.next_node.next_node
+      current.next_node = temp
+    end
+  end
+
+  def add_at(index, number)
     new_node = Node.new
     new_node.value = number
-    new_node.next_node=nil
-    
-    if index < 0 
-      return 
-    end
-    
-    if index.zero? 
+    new_node.next_node = nil
+
+    return if index < 0
+
+    if index.zero?
       new_node.next_node = head
       @head = new_node
-      return 
+      return
     end
-    
+
     if index == 1
       new_node.next_node = head.next_node
       @head.next_node = new_node
-    else 
+    else
       i = 0
-      current = head 
-      while i < index-1 && !current.next_node.nil? do 
+      current = head
+      while i < index - 1 && !current.next_node.nil?
         current = current.next_node
         i += 1
       end
       new_node.next_node = current.next_node
-      current.next_node= new_node
+      current.next_node = new_node
     end
   end
-end 
-
+end
 
 list = LinkedList.new
 
@@ -75,7 +99,7 @@ list.add_at(1, 11)
 list.add_at(0, 13)
 list.add_at(2, 17)
 list.add_at(6, 19)
-
+list.remove(4)
 
 puts list.get(0)
 puts list.get(1)
