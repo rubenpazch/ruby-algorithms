8982fbffc631e763b296933f200583bad49c4f19
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-05-25 11:36:55 -0500

    stack solve problem

--- LinkedList/3_Stack_linked_list.rb
+++ LinkedList/3_Stack_linked_list.rb
@@ -1,28 +1,56 @@
 # Start with your code from LinkedList challenge.
+class Node
+  attr_accessor :value, :next_node
+
+  def initialization(value, next_node = nil)
+    @values = value
+    @next_node = next_node
+  end
+end
 
 class Stack
-    def push(number)
-      # your code here
-    end
+  attr_accessor :head
+  def push(number)
+    new_node = Node.new
+    new_node.value  = number
+    new_node.next_node = nil
     
-    def pop
-      # your code here
+    if head.nil?
+      @head = new_node
+    else 
+      new_node.next_node = head
+      @head = new_node
     end
   end
   
-  stack = Stack.new
-  stack.push(3)
-  stack.push(5)
-  puts stack.pop
-  # => 5
-  
-  stack.push(2)
-  stack.push(7)
-  puts stack.pop
-  # => 7
-  
-  puts stack.pop
-  # => 2
-  
-  puts stack.pop
-  # => 3
+  def pop
+    value = nil
+    if head.nil? 
+      p 'empty'
+    else
+      value = head.value
+      temp  = head.next_node
+      @head = temp
+    end
+    value
+  end
+end
+
+stack = Stack.new
+stack.push(3)
+stack.push(5)
+
+puts stack.pop
+# => 5
+
+stack.push(2)
+stack.push(7)
+
+puts stack.pop
+# => 7
+
+puts stack.pop
+# => 2
+
+puts stack.pop
+# => 3
