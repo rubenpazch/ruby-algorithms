96e8580d0fd1a0028b4deae38124c19a19abe69f
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-07-24 06:16:07 -0500

    add faltter method

--- recursion/codequizzes.com/quiz.rb
+++ recursion/codequizzes.com/quiz.rb
@@ -4,8 +4,22 @@ def fib(number)
   elsif number == 1
     1
   else
-    fib(number - 1) + fib(number-2) 
+    fib(number - 1) + fib(number - 2)
   end
 end
 
-puts fib(6)
+#puts fib(6)
+
+def flatter(array, result = [])
+  array.each do |element|
+    if element.is_a?(Array)
+      flatter(element, result)
+    else
+      result << element
+    end
+  end
+  result
+end
+
+ #print flatter([[1, 2], [3, 4]])
+ #print flatter([1, [8, 9]], [3, 4])
