6fc0883ff45ca8d361e5f2ad7947a068f3ea3fb4
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-08-20 10:15:12 -0500

    practice hashes

---
+++ hash/effectiveWay.rb
@@ -0,0 +1,31 @@
+h = {apple: 100, orange: 200}
+
+#puts h.value?(100)
+#puts h.key?(:orange)
+#puts h[:apple]
+#puts h[:orange]
+#puts h.invert
+
+#persona = {
+#  nombre: "pedro",
+#  edad: 23,
+#  correo: "rubenpazchuspe@outlook.com"
+#}
+#persona.each { |x,y| puts "#{x} : #{y}"}
+
+#grades = { "Jane Doe" => 10, "Jim Doe" => 6 }
+#options = { :font_size => 10, :font_family => "Arial" }
+#options = { font_size: 10, font_family: "Arial" }
+#puts options[:font_size]
+
+#grades = Hash.new 
+#grades["Los Amigos"]  = 9
+#puts grades
+
+
+#grades = Hash.new(0)
+#puts grades
+
+#grades = {"Timmy Doe" => 8}
+grades.default = 0
+puts grades
