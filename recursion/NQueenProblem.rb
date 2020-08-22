65b3a18b357102c86bbb9f41057285588b144dd3
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-08-21 21:08:01 -0500

    queen recursive

---
+++ recursion/NQueenProblem.rb
@@ -0,0 +1,47 @@
+$N = 4
+$ld = Array.new(30)
+$rd = Array.new(30)
+$cl = Array.new(30)
+
+def printArray(arr) 
+  arr.each  do |x|
+        p x
+  end
+end
+
+def solveNQUtil(arr, col)
+  return true if col >= $N
+  
+  for i in 0..$N
+    if ($ld[i - col + $N -1] != 1 && $rd[i + col] != 1) && $cl[i] != 1
+      arr[i][col] = 1 
+      $ld[i - col + $N - 1] = $rd[i + col]
+      $cl[i] = 1
+
+      return true if solveNQUtil(arr, col + 1)
+
+      arr[i][col] = 0
+      $ld[i - col + N - 1] = $rd[i + col]
+      $cl[i] = 0
+        
+    end
+  end
+  return false
+end
+
+def solveNQ()
+  arr = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+  if solveNQUtil(arr, 0) == false
+    puts "solution does not exists"
+    return false
+  end
+  printArray(arr) 
+  return true
+end
+
+solveNQ()
+
+#arr = [[1,1,0,0],[0,1,1,0],[0,0,1,1],[0,0,0,1]]
+#p arr
+#solveNQUtil(arr, 2)
