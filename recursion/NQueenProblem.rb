24b9af66988420bb9e25d4b4d00ec9677b3d1093
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-08-26 15:44:47 -0500

    solving coding challenges

--- recursion/NQueenProblem.rb
+++ recursion/NQueenProblem.rb
@@ -3,43 +3,64 @@ $ld = Array.new(30)
 $rd = Array.new(30)
 $cl = Array.new(30)
 
-def printArray(arr)
+def safe?(arr, row, col)
+  (0..col - 1).each do |x|
+    return false if arr[row][x] == 1
+  end
+
+  r = row
+  s = col
+  r.downto(0) do |m|
+    return false if arr[m][s] == 1
+
+    s -= 1
+  end
+
+  j = row
+  k = col
+  j.downto(0) do |m|
+    return false if arr[m][k] == 1
+
+    k -= 1
+  end
+
+  true
+end
+
+def print_array(arr)
   arr.each do |x|
     p x
   end
 end
 
-def solveNQUtil(arr, col)
+def solve_nqutil(arr, col)
   return true if col >= $N
 
   (0..$N).each do |i|
-    next unless ($ld[i - col + $N - 1] != 1 && $rd[i + col] != 1) && $cl[i] != 1
+    next unless safe?(arr, i, col)
 
     arr[i][col] = 1
-    $ld[i - col + $N - 1] = $rd[i + col]
-    $cl[i] = 1
-
-    return true if solveNQUtil(arr, col + 1)
-
-    arr[i][col] = 0
-    $ld[i - col + N - 1] = $rd[i + col]
-    $cl[i] = 0
+    if solve_nqutil(arr, col + 1)
+      return true
+    else
+      arr[i][col] = 0
+    end
   end
   false
 end
 
-def solveNQ()
+def solve_nq()
   arr = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 
-  if solveNQUtil(arr, 0) == false
+  if solve_nqutil(arr, 0) == false
     puts 'solution does not exists'
     return false
   end
-  printArray(arr)
+  print_array(arr)
   true
 end
 
-solveNQ
+solve_nq()
 
 # arr = [[1,1,0,0],[0,1,1,0],[0,0,1,1],[0,0,0,1]]
 # p arr
