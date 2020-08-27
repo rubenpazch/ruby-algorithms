abd85a73f6e30bde982fee500b5479ffc782f8ea
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-08-26 23:05:40 -0500

    electronic shop

--- hackerrank/getMoneySpent.rb
+++ hackerrank/getMoneySpent.rb
@@ -6,5 +6,111 @@
 #   2  4  12  45
 #   5  12 44  111 2 7
 
-arr = [1,2,3,4]
-puts arr.max()
+#!/bin/ruby
+
+#
+# Complete the getMoneySpent function below.
+#
+def getMoneySpent(keyboards, drives, b)
+  arrK = keyboards.sort.reverse
+  arrD = drives.sort.reverse
+  
+  if keyboards.length == 1 &&  drives.length == 1 && (keyboards.sum + drives.sum) > b
+      return -1
+  end
+  
+  max = 0    
+  arrK.each do |k|
+      arrD.each do |d|
+          sum = k + d            
+          if sum <= b &&  sum > max
+              max = sum
+          end            
+      end
+  end
+  
+  return max
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+bnm = gets.rstrip.split
+
+b = bnm[0].to_i
+
+n = bnm[1].to_i
+
+m = bnm[2].to_i
+
+keyboards = gets.rstrip.split(' ').map(&:to_i)
+
+drives = gets.rstrip.split(' ').map(&:to_i)
+
+#
+# The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+#
+
+moneySpent = getMoneySpent keyboards, drives, b
+
+fptr.write moneySpent
+fptr.write "\n"
+
+fptr.close()
+
+
+
+
+
+
+#!/bin/ruby
+
+#
+# Complete the getMoneySpent function below.
+#
+def getMoneySpent(keyboards, drives, b)
+        
+    total = -1    
+    keyboards.each do |k|
+        drives.each do |d|
+            sum = k + d            
+            if sum <= b 
+                total = max(total, sum)
+            end            
+        end
+    end
+    
+    return total
+end
+
+def max(x, y )
+    if x > y 
+        return x 
+    else 
+        return y
+    end
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+bnm = gets.rstrip.split
+
+b = bnm[0].to_i
+
+n = bnm[1].to_i
+
+m = bnm[2].to_i
+
+keyboards = gets.rstrip.split(' ').map(&:to_i)
+
+drives = gets.rstrip.split(' ').map(&:to_i)
+
+#
+# The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+#
+
+moneySpent = getMoneySpent keyboards, drives, b
+
+fptr.write moneySpent
+fptr.write "\n"
+
+fptr.close()
