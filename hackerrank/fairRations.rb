fea49f089b3be877299a8018bd38b2bf2860e534
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-09-16 22:54:31 -0500

    solution for fair rations

---
+++ hackerrank/fairRations.rb
@@ -0,0 +1,39 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+# Complete the fairRations function below.
+def fairRations(b)
+  i = 0
+  counter = 0
+  while i < b.length    
+    
+    if b[i+1].nil? && b[i].odd?
+      return 'NO'
+    end
+    
+    if b[i].odd? && !b[i+1].nil?
+      b[i] += 1
+      b[i+1] += 1
+      counter += 2
+    end   
+    
+    i += 1 
+  end
+  
+  counter
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+N = gets.to_i
+
+B = gets.rstrip.split(' ').map(&:to_i)
+
+result = fairRations B
+
+fptr.write result
+fptr.write "\n"
+
+fptr.close()
