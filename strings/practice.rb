d564b18d855df1de2c52db590f97bc8c3d6b3f49
Ruben Paz <rubenpazchuspe@outlook.com>
2020-09-23 21:37:02 -0500

    add minimum coins

--- strings/practice.rb
+++ strings/practice.rb
@@ -18,3 +18,42 @@
 #
 #puts 3 / 2
 
+
+
+
+##cantidadACambiar = 1211
+#cantidadACambiar = 1000
+#cantidadACambiar = 100
+#cantidadACambiar = 10
+#cantidadACambiar = 1
+#cantidadACambiar = 0
+cantidadACambiar = 1123
+
+
+def totalBilletesMonedas (retirar)
+    billete100 = 100
+
+    arr = [10,1]
+    
+    minimo = 0
+    division = retirar/billete100
+    residuo = retirar%billete100 
+    minimo = minimo + division 
+
+    if residuo == 0 
+        return minimo
+    end
+    
+    i = 0
+    while  residuo != 0 
+        division = residuo/arr[i]
+        residuo = residuo%arr[i]
+        minimo  = minimo + division
+        i = i + 1
+    end
+
+    return minimo
+end
+
+
+p totalBilletesMonedas(cantidadACambiar)
