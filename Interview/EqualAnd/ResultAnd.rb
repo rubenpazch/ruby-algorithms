2ee066a388375c5042719fad2f84cc7b77fc7f86
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-03-23 11:45:55 -0500

    Conditions

--- Interview/EqualAnd/ResultAnd.rb
+++ Interview/EqualAnd/ResultAnd.rb
@@ -3,3 +3,12 @@ puts var1
 
 var2 = true && false
 puts var2
+
+true ? puts "true"  : puts "false"
+false ? puts "true"  : puts "false"
+nil ? puts "true"  : puts "false"
+1 ? puts "true"  : puts "false"
+0 ? puts "true"  : puts "false"
+"false" ? puts "true"  : puts "false"
+"" ? puts "true"  : puts "false"
+[] ? puts "true"  : puts "false"
