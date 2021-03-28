ebcbc6d8e42c53fcc2e20485390087b289c2e97c
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-03-28 18:13:27 -0500

    Singleton pattern

---
+++ patterns/singleton.rb
@@ -0,0 +1,21 @@
+class Singleton
+    @instance = new
+
+    private_class_method :new
+
+    def self.instance
+        @instance
+    end
+
+    def some_business_logic
+    end
+end
+
+s1 = Singleton.instance
+s2 = Singleton.instance
+
+if s1.equal?(s2)
+    print 'singleton works'
+else 
+    print 'singleton'
+end
