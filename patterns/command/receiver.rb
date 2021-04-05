56bb17316df8ba96eb067da98c6ede0025b38789
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-04-04 20:20:34 -0500

    add command pattern

---
+++ patterns/command/receiver.rb
@@ -0,0 +1,9 @@
+class Receiver
+    def do_something(a)
+        print "\nReceiver: Working on (#{a}.)"
+    end
+
+    def do_something_else(b)
+        print "\nReceiver: Also Working on (#{b}.)"
+    end
+end
