11537f16028b7203b143a76f2384806cff810d3a
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-04-19 09:08:39 -0500

    obejct oriented practices

---
+++ inheritance/PrivateExample2.rb
@@ -0,0 +1,23 @@
+class Person
+  def speak
+    puts "Hey douth!"
+  end
+
+  def whisper_louder
+    whisper
+  end
+
+  private
+  def whisper
+    puts "His name is not  doputhc"
+  end
+end
+
+
+you= Person.new
+you.speak
+
+a_hater = Person.new
+a_hater.speak
+# a_hater.whisper
+a_hater.whisper_louder
---
+++ inheritance/examples1.rb
@@ -0,0 +1,19 @@
+class ParentClass
+  def method1
+    puts "hello from method 1 in the parent class"
+  end
+
+  def method2
+    puts "hello from method2 in the parent class"
+  end
+end
+
+class ChildClass < ParentClass
+  def method2
+    puts "hello from method 2 in child class"
+  end
+end
+
+my_object = ChildClass.new
+my_object.method1
+my_object.method2
