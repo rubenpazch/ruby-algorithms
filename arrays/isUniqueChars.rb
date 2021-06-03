8f78b3dc522b2d9dc871f6e0e94d30d4eac566cd
Ruben Paz Chuspe <ruben.paz@vascarsolutions.com>
2021-06-03 04:39:56 -0500

    check is string is unique

--- arrays/isUniqueChars.rb
+++ arrays/isUniqueChars.rb
@@ -1,5 +1,14 @@
 def isUniqueChar?(str)
     return false if str.length > 128
     arr = Array.new(128, false)
-     
+
+    str.each_byte do |byte|
+        if arr[byte]
+            return false 
+        end 
+        arr[byte]=true
+    end
+    return true
 end
+
+p isUniqueChar?('casi')
